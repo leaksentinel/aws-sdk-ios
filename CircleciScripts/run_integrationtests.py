3d5824a8962e13f51f062bb3355fa7fbf199923d
sunchunqiang <sdechunq@amazon.com>
2019-03-04 08:43:57 -0800

    run integration test

--- CircleciScripts/run_integrationtests.py
+++ CircleciScripts/run_integrationtests.py
@@ -24,14 +24,25 @@ def getfailedcases():
             failedtests.add(testbundle + '/' + suitename + '/' + casename)
     return failedtests 
 
-
+def getfailedcasesNoBundle():   
+    xmlfile='build/reports/junit.xml'
+    tree = ET.parse(xmlfile) 
+    root = tree.getroot() 
+    failedtests = set()
+    #TODO  we can filter with condtion 
+    for testsuite in root.findall(".//testsuite"):  
+        for testcase in testsuite.findall('.//testcase[failure]'): 
+            suitename = testsuite.get('name')
+            casename = testcase.get('name')
+            failedtests.add(suitename + '/' + casename)
+    return failedtests 
 
 
  #run test   
-def runtest(otherargments, timeout = 0):
+def runtest(otherargments, projectPath, schemeName, projectName, destination, derivedDataPath, timeout = 0):
     runcommand("rm raw.log")
     runcommand("rm xcpretty.log")
-    testcommand = "xcodebuild   test-without-building  -project AWSiOSSDKv2.xcodeproj -scheme AWSAllTests -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 8,OS=12.1'" 
+    testcommand = "xcodebuild   test-without-building  -project {0} -scheme {1} -sdk iphonesimulator -destination '{2}'  -derivedDataPath  {3}/{4}".format(projectPath,schemeName, destination, derivedDataPath, projectName) 
     testcommand +=" " +  otherargments;
     rawoutput = open('raw.log','w')
     exit_code = runcommand(testcommand,timeout, pipeout = rawoutput)
@@ -39,19 +50,21 @@ def runtest(otherargments, timeout = 0):
     print("Formatting test result .......")
     xcprettycommand = "cat raw.log | xcpretty -r junit  | tee xcpretty.log"
     runcommand(xcprettycommand)
-    return exit_code
-
+    return exit_code    
 
 ##########################  main function ###############################
 # a command will like 
 
 if (len(sys.argv) < 3 or sys.argv[1] == '-h' or sys.argv[1] == '-h') : 
-    print("Usage: \r\n {0} <integrationTestsConfiguration json file path> <test result location>".format(sys.argv[0])) ;
+    print("Usage: \r\n {0} <integrationTestsConfiguration json file path> <test result location> <group name>".format(sys.argv[0])) ;
     exit(1)
 
 
 jsonfilename=sys.argv[1]
 test_result_folder=sys.argv[2]
+group_name = sys.argv[3]
+destination = sys.argv[4]
+derivedDataPath = sys.argv[5]
 
 with open(jsonfilename, 'r') as jsonfile:
     jsonstring = jsonfile.read()
@@ -59,20 +72,25 @@ with open(jsonfilename, 'r') as jsonfile:
 testConfigure =  demjson.decode(jsonstring)
 runningConfigure = testConfigure['runningConfigure']
 projectName = runningConfigure['projectName']
+projectPath = runningConfigure['projectPath']
 schemeName = runningConfigure['schemeName']
 sdkName = runningConfigure['sdkName']
-paltformName = runningConfigure['paltformName']
-deviceName = runningConfigure['deviceName']
-osVersion = runningConfigure['osVersion']
+ 
 
-buildcommand = f"xcodebuild  build-for-testing -project {projectName} -scheme {schemeName} -sdk {sdkName} -destination 'platform={paltformName},name={deviceName},OS={osVersion}'"      
-exit_code = runcommand(buildcommand)
-if(exit_code != 0):
-    exit(exit_code)  
+print("group name:", group_name)
+testgroup = testConfigure[group_name]
+testlist = testgroup['test_list']
+if 'projectName' in testgroup.keys() :
+    projectName = testgroup['projectName']
+if 'projectPath' in testgroup.keys():
+    projectPath = testgroup['projectPath']
+if 'schemeName' in testgroup.keys():
+    schemeName = testgroup['schemeName']
 
-testlist = testConfigure['testList']
-testcommandhead = f"xcodebuild   test-without-building  -project {projectName} -scheme {schemeName} -sdk {sdkName} -destination 'platform={paltformName},name={deviceName},OS={osVersion}'"
-testcommandtail = "    | tee raw.log  | xcpretty -r junit | tee xcpretty.log"
+print("projectName, projectPath, schemeName, destination", projectName, projectPath, schemeName, destination)
+# testcommandhead = f"xcodebuild   test-without-building  -project {projectName} -scheme {schemeName} -sdk {sdkName} -destination 'platform={paltformName},name={deviceName},OS={osVersion}'"
+# testcommandtail = "    | tee raw.log  | xcpretty -r junit | tee xcpretty.log"
+runcommand('echo "export testresult=0" >> $BASH_ENV')
 for testname in testlist:
 
     print("-------------------------------", testname , "-------------------------------");
@@ -81,14 +99,19 @@ for testname in testlist:
     testarguments = ' -only-testing:' + testname
     #create skipping tests parameters 
     skipingtests = ""
-    if 'skipingtests' in test:
-        for skipingtest in test['skipingtests']:
+    if 'excludetests' in test:
+        for skipingtest in test['excludetests']:
             skipingtests += ' -skip-testing:' + testname+ "/" + skipingtest
-
-    exit_code = runtest(testarguments + skipingtests)
+        print("skipingtests:", skipingtests)
+    for index in range(3):
+        print("index:", index)
+        exit_code = runtest(testarguments + skipingtests, projectPath, schemeName, projectName, destination, derivedDataPath)
+        if exit_code == 0 or exit_code == 65 :
+            break
+        time.sleep(30)
     print(testname, "exit code:", exit_code)
     # if test fails, check if the failed tests can be retried
-    if exit_code != 0:
+    if exit_code == 65:
         if 'retriabletests' in test:
             retriableset = set()
             suitesname = testname;
@@ -122,7 +145,7 @@ for testname in testlist:
                 for failed in faileds:
                     testarguments += ' -only-testing:' + failed
                 retrytimes += 1
-                exit_code = runtest(testarguments, timeout);
+                exit_code = runtest(testarguments,projectPath, schemeName, projectName, destination, derivedDataPath);
                 print("retry exit code:", exit_code)                
                 if(exit_code != 0 ):
                     faileds = getfailedcases()
@@ -132,4 +155,21 @@ for testname in testlist:
         runcommand('echo "{2}" >> {0}/{1}/exitcode.log'.format(test_result_folder,testname,exit_code))
         runcommand('mv raw.log {0}/{1}/raw.log'.format(test_result_folder,testname))
         runcommand('mv xcpretty.log {0}/{1}/xcpretty.log'.format(test_result_folder,testname))
-        runcommand('mv build/reports/junit.xml {0}/{1}/junit.xml'.format(test_result_folder,testname))
+        runcommand('cp build/reports/junit.xml {0}/{1}/junit.xml'.format(test_result_folder,testname))
+        if exit_code != 65 :
+            runcommand('echo "export testresult=1" >> $BASH_ENV')
+        else :
+            failedtests = getfailedcasesNoBundle() 
+            print("failedtests:", failedtests)  
+            if 'ignoreFailures' in test:
+                ignoreFailures = test['ignoreFailures']                
+                for test in failedtests:
+                    if test not in ignoreFailures:
+                        print("{0} is not in the ignorefailures list".format(test))
+                        runcommand('echo "export testresult=1" >> $BASH_ENV')
+                        break;
+            else:
+                print("There are failed test cases")
+                runcommand('echo "export testresult=1" >> $BASH_ENV')
+
+
