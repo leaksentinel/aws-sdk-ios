1154cf9c9d5add1649584a8c1b57f31b9482741a
jkeiththomson <keith@leaksentinel.com>
2022-12-29 17:54:37 -0800

    Prevent screen from jumping when kybd is dismissed

--- AWSAuthSDK/Sources/AWSAuthUI/AWSSignInViewController.m
+++ AWSAuthSDK/Sources/AWSAuthUI/AWSSignInViewController.m
@@ -111,21 +111,9 @@ static NSInteger const SCALED_DOWN_LOGO_IMAGE_HEIGHT = 100;
 
 
 - (void)keyboardDidShow:(NSNotification *)notification {
-    CGSize keyboardSize = ((NSValue *)[[notification userInfo]
-                                       valueForKey:UIKeyboardFrameBeginUserInfoKey]).CGRectValue.size;
-    
-    CGPoint buttonOrigin = self.signInButton.frame.origin;
-    CGRect visibleRect = self.view.frame;
-    
-    visibleRect.size.height -= keyboardSize.height;
-    
-    if (visibleRect.size.height < buttonOrigin.y) {
-        [self.view setFrame:CGRectMake(0,visibleRect.size.height - buttonOrigin.y, self.view.frame.size.width, self.view.frame.size.height)];
-    }
 }
 
 - (void)keyboardDidHide:(NSNotification *)notification {
-    [self.view setFrame:CGRectMake(0, NAVIGATION_BAR_HEIGHT ,self.view.frame.size.width,self.view.frame.size.height)];
 }
 
 - (void)viewDidLoad {
